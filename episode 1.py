65bafed013ba726868deded37edd5641c6b3e559
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-04-21 23:15:58 +0000

    creating color library

--- episode 1.py
+++ episode 1.py
@@ -3,6 +3,15 @@
 import pygame 
 pygame.init()
 
+# color library (added in for most pygame)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+white = (255, 255, 255)
+black = (0, 0, 0)
+purple = (127, 0, 255)
+orange = (255, 165, 0)
+
 screen = pygame.display.set_mode([300, 450])
 pygame.display.set_Caption('Not Adventure Capitalist (for legal reasons)')
 
