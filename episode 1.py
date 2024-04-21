d27aacdb009254d750b7d616d1c2c7961e822d1f
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-04-21 23:11:47 +0000

    Make the game run

--- episode 1.py
+++ episode 1.py
@@ -1,1 +1,17 @@
 # recreation of idle capitalist
+
+import pygame 
+pygame.init()
+
+
+
+
+running = True
+while running:
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
+
+pygame.quit()
