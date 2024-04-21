6e287d5a159505b4d75454c852d7953ce30a30d9
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-04-21 23:17:12 +0000

    Basic settings background time

--- episode 1.py
+++ episode 1.py
@@ -14,6 +14,10 @@ orange = (255, 165, 0)
 
 screen = pygame.display.set_mode([300, 450])
 pygame.display.set_Caption('Not Adventure Capitalist (for legal reasons)')
+background = black
+framerate = 60
+font = pygame.font.Font('freesansbold.ttf', 16)
+timer = pygame.timer.Clock()
 
 
 running = True
