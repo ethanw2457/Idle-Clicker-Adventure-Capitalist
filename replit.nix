0c1d47339e17cbe08a26c81b9b8b47b04c1a9b62
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-04-25 05:09:25 +0000

    update

--- replit.nix
+++ replit.nix
@@ -1,5 +1,15 @@
 { pkgs }: {
     deps = [
+      pkgs.portmidi
+      pkgs.pkg-config
+      pkgs.libpng
+      pkgs.libjpeg
+      pkgs.freetype
+      pkgs.fontconfig
+      pkgs.SDL2_ttf
+      pkgs.SDL2_mixer
+      pkgs.SDL2_image
+      pkgs.SDL2
       pkgs.cowsay
     ];
 }
